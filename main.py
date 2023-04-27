b5819aad57f95e67d18c33f933a3ddfe787c1a99
Hoang <49335521+hoangxtr@users.noreply.github.com>
2023-04-27 23:06:20 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,49 @@
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from prefect import flow, task
+import pandas as pd
+import numpy as np
+
+@task
+def fetch_data():
+    csv_url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    data = pd.read_csv(csv_url, sep=";")
+    return data
+
+@task
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
+
+@task
+def train_model(data, alpha=0.5, l1_ratio=0.5):
+
+    train, test = train_test_split(data)
+
+    # The predicted column is "quality" which is a scalar from [3, 9]
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+    rmse, mae, r2 = eval_metrics(test_y, predicted_qualities)
+
+    # Print out metrics
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+
+@flow
+def main():
+  data = fetch_data()
+  train_model(data)
